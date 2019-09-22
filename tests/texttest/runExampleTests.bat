1f4d18629d150baddec2a4b5929652bced8d6782
dkrajzew <d.krajzewicz@gmail.com>
2019-09-22 23:07:13 +0200

    adding tests

---
+++ tests/texttest/runExampleTests.bat
@@ -0,0 +1,4 @@
+set EXAMPLE_BINARY=java -classpath optionslib.jar %CD%\..\..\bin\optionslib.jar de.dks.examples.options.Example
+set PATH=%PATH%;C:\Program Files (x86)\TkDiff;C:\MehrProgramme\texttest-3.19\source\bin
+set TEXTTEST_HOME=%CD%
+C:\Python27_32\python.exe C:\MehrProgramme\texttest\source\bin\texttest.py -a example -gx
