484e9f55b1be3ad081babc837d92999e713ad860
dkrajzew <d.krajzewicz@gmail.com>
2021-07-31 21:17:41 +0200

    patching after recent changes

--- tests/texttest/runAllTests.bat
+++
@@ -1,5 +0,0 @@
-set EXAMPLE_BINARY=%CD%\..\..\bin\example.exe
-set TESTER_BINARY=%CD%\..\..\bin\tester.exe
-set PATH=%PATH%;C:\Program Files (x86)\TkDiff;C:\MehrProgramme\texttest-3.19\source\bin
-set TEXTTEST_HOME=%CD%
-C:\Python27_32\python.exe C:\MehrProgramme\texttest\source\bin\texttest.py -gx
